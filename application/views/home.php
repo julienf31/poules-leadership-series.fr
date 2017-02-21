32f36079906cd1655c523e84b628e157bf2f4e00
Julien <julien.fournier@ynov.com>
2017-02-21 14:40:12 +0100

    gitignore

---
+++ application/views/home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+</head>
+<body>
+
+<div id="container">
+	Page home
+</div>
+
+</body>
+</html>
